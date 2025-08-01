b09365a7770b6050095b8463b3eeb05e2c37ae6a
Jimy Sebastian Angarita Triana <jimy_sangarita@soy.sena.edu.co>
2025-08-01 14:21:31 -0500

    Solucion Ejercicio 2

---
+++ Ejercicio 2.py
@@ -0,0 +1,22 @@
+def Edad (edad):
+    """
+   Esta función toma el peso de la persona y la divide sobre la altura elevada a la 2.
+
+   Args:
+       edad (int): El primer valor.
+
+   Returns:
+       int: Edad de la persona.
+       """
+    return  edad
+
+edad = int(input("Ingrese su Edad: "))
+
+result = Edad(edad)
+
+if result <=17 :
+    print(f"Su edad es: {result} es un niño y es menor de edad")
+elif result >= 18 and result <= 25 :
+    print(f"Su edad es: {result} y es un joven y es mayor de edad")
+else :
+    print(f"Su edad es: {result} y es un adulto y es mayor de edad")
