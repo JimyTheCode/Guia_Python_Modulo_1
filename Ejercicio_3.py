9d99d37e6dd9044888a49e84d4931c9caaf64b78
Jimy Sebastian Angarita Triana <jimy_sangarita@soy.sena.edu.co>
2025-08-01 16:00:09 -0500

    Solucion Ejercicio 3

---
+++ Ejercicio_3.py
@@ -0,0 +1,32 @@
+def Frase_usario (Frase):
+    """
+   Esta función toma la una frase que digitará el usuario, borra los espacios para no tenerlos en cuenta, no permite ingresar simbolos.
+
+   Args:
+       Frase (str): frase que ingresa el usuario.
+
+   Returns:
+       dict: numero de vocales y de consonantes de la frase.
+       """
+
+    vocales = ["a", "e", "i", "o", "u"]
+    contadorV = 0
+    contadorC = 0
+    Frase_Nueva = Frase.replace(" ", "").lower()
+
+    for i in Frase_Nueva:
+        if ord(i) >= 97 and ord(i) <= 122 :
+            if i in vocales:
+                contadorV += 1
+            else:
+                contadorC += 1
+        else:
+            print(f"{i} no es un caracter valido")
+
+    return { "vocales" : contadorV, "consonantes" : contadorC }
+
+Frase = input("Ingrese su frase: ")
+resultado = Frase_usario(Frase)
+
+print(f"El total de vocales es: {resultado['vocales']}")
+print(f"El total de consonantes:  {resultado['consonantes']}")
